7e1bb774d996b0b4fc3614d3238acf9eaae14e59
SSSerg05 <ssserg05@gmail.com>
2023-04-28 00:25:06 +0300

    temp
    Signed-off-by: SSSerg05 <ssserg05@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+// получить данные
+function getWeather() {
+  // Функция getWeather получает из select id города.
+  const cityId = outSelect.value;
+  localStorage.setItem('selectCity', outSelect.selectedIndex);
+
+  fetch(`${param.url}weather?id=${cityId}&units=metric&APPID=${param.appid}`)
+.then(weather => {
+    return weather.json();
+  }).then(showWeather);
+}
