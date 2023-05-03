a2a2e5ea1993702cbbb1fbc9512fb7b6fef6d91a
SSSerg05 <ssserg05@gmail.com>
2023-05-03 12:41:45 +0300

    add lodash.debounce

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
+// name.official - полное имя страны
+// capital - столица
+// population - население
+// flags.svg - ссылка на изображение флага
+// languages - массив языков
+const param = {
+  url: 'https://restcountries.com/v3.1/all',
+  fields: ['name.official', 'capital', 'population', 'flags.svg', 'languages'],
+};
+
+const refs = {
+  input: document.querySelector('#search-box'),
+  list: document.querySelector('.country-list'),
+  out: document.querySelector('.country-info'),
+}
+
+
 // получить данные
-function getWeather() {
-  // Функция getWeather получает из select id города.
+function getCountries() {
+  // Функция getCountries получает из select id города.
   const cityId = outSelect.value;
   localStorage.setItem('selectCity', outSelect.selectedIndex);
 
-  fetch(`${param.url}weather?id=${cityId}&units=metric&APPID=${param.appid}`)
-.then(weather => {
-    return weather.json();
-  }).then(showWeather);
+  fetch(`${param.url}?fields=${[...param.fields]}`)
+    .then(country => {
+      return country.json();
+    }).then(showCountry);
 }
